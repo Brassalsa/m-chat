36796159eb10f944685bd5e36bea6196b4fd7fc4
Brassalsa <sauravchauhan2k@gmail.com>
2024-06-06 13:20:42 +0530

    ready template and responses

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+type Page struct {
+	Payload string
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	s := newServer(":3000", "static")
+
+	s.RegisterHandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := Page{
+			Payload: "hello world",
+		}
+		respondTempl(w, 200, "index", data)
+	})
+
+	log.Printf("serving @ http://loclahost:%s\n", s.ListenAddr)
+	log.Fatal(s.Listen())
 }
