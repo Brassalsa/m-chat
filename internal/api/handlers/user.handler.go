dab143b02ef6b0c5aa509724c4a231d1c4e56b57
Brassalsa <sauravchauhan2k@gmail.com>
2024-06-13 19:13:20 +0530

    user handler changes

--- internal/api/handlers/user.handler.go
+++ internal/api/handlers/user.handler.go
@@ -1,20 +1,134 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/Brassalsa/m-chat/internal/types"
 	"github.com/Brassalsa/m-chat/pkg"
+	"github.com/Brassalsa/m-chat/pkg/helpers"
+	"github.com/Brassalsa/m-chat/pkg/res"
 )
 
 type UserHandler struct {
-	Rmux *http.ServeMux
+	types.Handler
+}
+
+type LoginUser struct {
+	Email string `bson:"email"`
 }
 
 func (u *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
+
+	formData := res.NewFormData()
 	email := r.FormValue("email")
 	password := r.FormValue("password")
-	fmt.Printf("email %s, password %s\n", email, password)
-	pkg.RespondTempl(w, 200, "sign-in", "")
+	formData.Values["email"] = email
+	formData.Values["password"] = password
+	resCode := 200
+	defer func() {
+		pkg.RespondTempl(w, resCode, "log-in", formData)
+	}()
+
+	user := types.User{}
+	err := u.Db.FindOne(u.Coll, types.Email{
+		Email: email,
+	}, &user)
+
+	if err != nil {
+		formData.Errors["form"] = err.Error()
+		return
+	}
+
+	if user.Email == "" {
+		formData.Errors["email"] = "email not found"
+		return
+	}
+
+	// check password
+	if !pkg.CompareHash(user.Password, password) {
+		formData.Errors["password"] = "wrong password"
+		return
+	}
+
+	// TODO: header tokens, etc
+}
+
+type RegisterUser struct {
+	Email    string `json:"email" bson:"email"`
+	Username string `json:"username" bson:"username"`
+	Name     string `json:"name" bson:"name"`
+	Password string `bson:"password"`
+}
+
+func (u *UserHandler) Regsiter(w http.ResponseWriter, r *http.Request) {
+	formData := res.NewFormData()
+
+	username := r.FormValue("username")
+	formData.Values["username"] = username
+
+	email := r.FormValue("email")
+	formData.Values["email"] = email
+
+	name := r.FormValue("name")
+	formData.Values["name"] = name
+
+	password := r.FormValue("password")
+	formData.Values["password"] = password
+
+	confirmPassword := r.FormValue("confirm_password")
+	formData.Values["confirm_password"] = confirmPassword
+
+	resCode := 200
+
+	defer func() {
+		pkg.RespondTempl(w, resCode, "register", formData)
+	}()
+
+	if helpers.CheckEmpty([]string{username, email, password}) {
+		formData.Errors["form"] = "username, email and password are required"
+		return
+	}
+
+	if password != confirmPassword {
+		formData.Errors["confirm_password"] = "passwords don't match"
+		return
+	}
+
+	user := types.User{}
+
+	if err := u.Db.FindOne(u.Coll,
+		u.Db.Or(
+			types.Username{Username: username},
+			types.Email{Email: email}),
+		&user,
+	); err != nil {
+		formData.Errors["form"] = err.Error()
+		return
+	}
+
+	if user.Username != "" && user.Username == username {
+		formData.Errors["username"] = "username already taken"
+		return
+	}
+
+	if user.Email != "" && user.Email == email {
+		formData.Errors["email"] = "email already exists"
+		return
+	}
+
+	pkg.HashString(&password)
+
+	if err := u.Db.InsertTo(u.Coll, RegisterUser{
+		Username: username,
+		Email:    email,
+		Name:     name,
+		Password: password,
+	}); err != nil {
+
+		formData.Errors["form"] = err.Error()
+		return
+	}
+
+	// TODO: header tokens, etc
 
 }
